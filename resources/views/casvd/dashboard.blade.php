d61cc99407c9fa12583e1d10224e0e5c5cc4bb7b
minhld5-m <49550150+Minhld5@users.noreply.github.com>
2020-12-08 14:49:32 +0700

    Trans

--- resources/views/casvd/dashboard.blade.php
+++ resources/views/casvd/dashboard.blade.php
@@ -2,9 +2,6 @@
 @section('content')
 @include('casvd.menu')
 
-<style>
-
-</style>
 
 <!-- Page header -->
 <div class="page-header">
@@ -15,7 +12,7 @@
 </div>
 
 <!--=== Statboxes ===-->
-<div class="row row-bg"> <!-- .row-bg -->
+<!-- <div class="row row-bg">
 	<div class="col-md-12">
 		<div class="widget box" style="display:grid; grid-template-columns: 1fr;">
 			<div class="widget-header" style="display:grid; grid-template-columns: 0px fit-content(800px) fit-content(800px) fit-content(800px) fit-content(300px);">
@@ -59,14 +56,13 @@
 						</div>
 						<div class="widget-content" style="display:grid; grid-template-columns:1fr 1fr; grid-template-rows:1fr 1fr;">
 							<div style="padding: 0px; margin: 0px; grid-row: 1/3; grid-column: 1/2;">
-								<!-- <img src="{{@Config::get('app.url')}}/images/ciscosdwan/wanedge.png" style="width: 50px;"> -->
 								<h3><b>Total</b></h3>
 							</div>
 							<div class="title" style="grid-row: 1/2; grid-column: 2/3;">Total</div>
 							<div class="value" id="incidentcount" style="grid-row: 2/3; grid-column: 2/3;">N/A</div>
 						</div>
-					</div> <!-- /.smallstat -->
-				</div> <!-- /.col-md-2 -->
+					</div>
+				</div>
 
 				<div class="col-md-4">
 					<div class="statbox widget box box-shadow" style="margin: 0;">
@@ -75,14 +71,13 @@
 						</div>
 						<div class="widget-content" style="display:grid; grid-template-columns:1fr 1fr; grid-template-rows:1fr 1fr;">
 							<div style="padding: 0px; margin: 0px; grid-row: 1/3; grid-column: 1/2;">
-								<!-- <img src="{{@Config::get('app.url')}}/images/ciscosdwan/wanedge.png" style="width: 50px;"> -->
 								<h3><b>Total</b></h3>
 							</div>
 							<div class="title" style="grid-row: 1/2; grid-column: 2/3;">Total</div>
 							<div class="value" id="requestcount" style="grid-row: 2/3; grid-column: 2/3;">N/A</div>
 						</div>
-					</div> <!-- /.smallstat -->
-				</div> <!-- /.col-md-2 -->
+					</div>
+				</div>
 
 				<div class="col-md-4 hidden-xs">
 					<div class="statbox widget box box-shadow" style="margin: 0;">
@@ -91,148 +86,252 @@
 						</div>
 						<div class="widget-content" style="display:grid; grid-template-columns:1fr 1fr; grid-template-rows:1fr 1fr;">
 							<div style="padding: 0px; margin: 0px; grid-row: 1/3; grid-column: 1/2;">
-								<!-- <img src="{{@Config::get('app.url')}}/images/ciscosdwan/wanedge.png" style="width: 50px;"> -->
 								<h3><b>Total</b></h3>
 							</div>
 							<div class="title" style="grid-row: 1/2; grid-column: 2/3;">Total</div>
 							<div class="value" id="changecount" style="grid-row: 2/3; grid-column: 2/3;">N/A</div>
 						</div>
-					</div> <!-- /.smallstat -->
-				</div> <!-- /.col-md-2 -->
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
 	
 
-</div> <!-- /.row -->
+</div> -->
 <!-- /Statboxes -->
 
-<!--=== Top10 ===-->
-	<div class="row">
-		<!-- Top10 Incidents -->
-		<div class="col-md-4">
-			<div class="widget box">
-				<div class="widget-header">
-					<h4><i class="icon-reorder"></i>Top 10 open incidents</h4>
-					<div class="toolbar no-padding">
-						<div class="btn-group">
-							<span class="btn btn-xs widget-collapse"><i class="icon-angle-down"></i></span>
-						</div>
-					</div>
+<!--=== Statboxes ===-->
+<div class="row row-bg" style="display: flex; justify-content: space-around;">
+	<div class="col-md-3">
+		<div class="statbox widget box box-shadow">
+			<div class="widget-header" style="display: grid; grid-template-columns: 60px 1fr;">
+				<span style="grid-column: 1/2;"><b>INCIDENT</b></span>
+				<div id="incidentrange" style="cursor: pointer; grid-column: 2/3; text-align: right;">
+					<span></span> &nbsp;
+					<i class="fa fa-calendar"></i>&nbsp;
+					<i class="fa fa-caret-down"></i>
 				</div>
-				<div class="widget-content" style="vertical-align: middle;">
-					<div class="ct-control-status" style="overflow-x: overlay; border:none;" align="center">
-						<!-- <div id="mytree"></div> -->
-						<div id="ajaxcasvddashboardincidents"></div>
-					</div>
-				</div>
-
 			</div>
-		</div>
-
-		<!-- Top10 Requests -->
-		<div class="col-md-4">
-			<div class="widget box">
-				<div class="widget-header">
-					<h4><i class="icon-reorder"></i>Top 10 open requests</h4>
-					<div class="toolbar no-padding">
-						<div class="btn-group">
-							<span class="btn btn-xs widget-collapse"><i class="icon-angle-down"></i></span>
-						</div>
-					</div>
+			<div class="widget-content">
+				<div class="visual" style="padding: 0px; margin: 0px;">
+					<img src="{{@Config::get('app.url')}}/images/casvd/incident.png" style="width: 50px;">
 				</div>
-				<div class="widget-content" style="vertical-align: middle;">
-					<div class="ct-control-status" style="overflow-x: overlay; border:none;" align="center">
-						<!-- <div id="mytree"></div> -->
-						<div id="ajaxcasvddashboardrequests"></div>
-					</div>
-				</div>
-
-			</div>
-		</div>
-
-		<!-- Top10 Changes -->
-		<div class="col-md-4">
-			<div class="widget box">
-				<div class="widget-header">
-					<h4><i class="icon-reorder"></i>Top 10 open changes</h4>
-					<div class="toolbar no-padding">
-						<div class="btn-group">
-							<span class="btn btn-xs widget-collapse"><i class="icon-angle-down"></i></span>
-						</div>
-					</div>
-				</div>
-				<div class="widget-content" style="vertical-align: middle;">
-					<div class="ct-control-status" style="overflow-x: overlay; border:none;" align="center">
-						<!-- <div id="mytree"></div> -->
-						<div id="ajaxcasvddashboardchanges"></div>
-					</div>
-				</div>
-
+				<div class="title">Total</div>
+				<div class="value" id="incidentcount">N/A</div>
 			</div>
 		</div>
 	</div>
+	<div class="col-md-3">
+		<div class="statbox widget box box-shadow">
+			<div class="widget-header" style="display: grid; grid-template-columns: 60px 1fr;">
+				<span style="grid-column: 1/2;"><b>REQUEST</b></span>
+				<div id="requestrange" style="cursor: pointer; grid-column: 2/3; text-align: right;">
+					<span id="requestrangespan"></span> &nbsp;
+					<i class="fa fa-calendar"></i>&nbsp;
+					<i class="fa fa-caret-down"></i>
+				</div>
+			</div>
+			<div class="widget-content">
+				<div class="visual" style="padding: 0px; margin: 0px;">
+					<img src="{{@Config::get('app.url')}}/images/casvd/request.png" style="width: 50px;">
+				</div>
+				<div class="title">Total</div>
+				<div class="value" id="requestcount">N/A</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="statbox widget box box-shadow">
+			<div class="widget-header" style="display: grid; grid-template-columns: 60px 1fr 1fr;">
+				<span style="grid-column: 1/2;"><b>CHANGE</b></span>
+				<div id="changerange" style="cursor: pointer; grid-column: 2/4; text-align: right;">
+					<!-- <span></span> &nbsp; -->
+					<input type="text" class="form-control" style="border: none; outline: none; background: transparent;" disabled="disabled"></input>
+					<i class="fa fa-calendar"></i>&nbsp;
+					<i class="fa fa-caret-down"></i>
+				</div>
+			</div>
+			<div class="widget-content">
+				<div class="visual" style="padding: 0px; margin: 0px;">
+					<img src="{{@Config::get('app.url')}}/images/casvd/change.png" style="width: 50px;">
+				</div>
+				<div class="title">Total</div>
+				<div class="value" id="changecount">N/A</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- /Statboxes -->
+
+<!--=== Top10 ===-->
+<div class="row">
+	<!-- Top10 Incidents -->
+	<div class="col-md-4">
+		<div class="widget box">
+			<div class="widget-header">
+				<h4><i class="icon-reorder"></i>Top 10 open incidents</h4>
+				<div class="toolbar no-padding">
+					<div class="btn-group">
+						<span class="btn btn-xs widget-collapse"><i class="icon-angle-down"></i></span>
+					</div>
+				</div>
+			</div>
+			<div class="widget-content" style="vertical-align: middle;">
+				<div class="ct-control-status" style="overflow-x: overlay; border:none;" align="center">
+					<!-- <div id="mytree"></div> -->
+					<div id="ajaxcasvddashboardincidents"></div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<!-- Top10 Requests -->
+	<div class="col-md-4">
+		<div class="widget box">
+			<div class="widget-header">
+				<h4><i class="icon-reorder"></i>Top 10 open requests</h4>
+				<div class="toolbar no-padding">
+					<div class="btn-group">
+						<span class="btn btn-xs widget-collapse"><i class="icon-angle-down"></i></span>
+					</div>
+				</div>
+			</div>
+			<div class="widget-content" style="vertical-align: middle;">
+				<div class="ct-control-status" style="overflow-x: overlay; border:none;" align="center">
+					<!-- <div id="mytree"></div> -->
+					<div id="ajaxcasvddashboardrequests"></div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<!-- Top10 Changes -->
+	<div class="col-md-4">
+		<div class="widget box">
+			<div class="widget-header">
+				<h4><i class="icon-reorder"></i>Top 10 open changes</h4>
+				<div class="toolbar no-padding">
+					<div class="btn-group">
+						<span class="btn btn-xs widget-collapse"><i class="icon-angle-down"></i></span>
+					</div>
+				</div>
+			</div>
+			<div class="widget-content" style="vertical-align: middle;">
+				<div class="ct-control-status" style="overflow-x: overlay; border:none;" align="center">
+					<!-- <div id="mytree"></div> -->
+					<div id="ajaxcasvddashboardchanges"></div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
 <!-- /Top10 -->
 
 <script>
-		setInterval(function(){
-			var ajaxcasvddashboardincidents = '<?php echo URL::route('ajaxcasvddashboardincidents') ?>';
-			$('#ajaxcasvddashboardincidents').load(ajaxcasvddashboardincidents).fadeIn("slow");
+	setInterval(function () {
+		var ajaxcasvddashboardincidents = '<?php echo URL::route('ajaxcasvddashboardincidents') ?>';
+		$('#ajaxcasvddashboardincidents').load(ajaxcasvddashboardincidents).fadeIn("slow");
 
-			var ajaxcasvddashboardrequests = '<?php echo URL::route('ajaxcasvddashboardrequests') ?>';
-			$('#ajaxcasvddashboardrequests').load(ajaxcasvddashboardrequests).fadeIn("slow");
+		var ajaxcasvddashboardrequests = '<?php echo URL::route('ajaxcasvddashboardrequests') ?>';
+		$('#ajaxcasvddashboardrequests').load(ajaxcasvddashboardrequests).fadeIn("slow");
 
-			var ajaxcasvddashboardchanges = '<?php echo URL::route('ajaxcasvddashboardchanges') ?>';
-			$('#ajaxcasvddashboardchanges').load(ajaxcasvddashboardchanges).fadeIn("slow");
-		},{{$refreshrate}});
+		var ajaxcasvddashboardchanges = '<?php echo URL::route('ajaxcasvddashboardchanges') ?>';
+		$('#ajaxcasvddashboardchanges').load(ajaxcasvddashboardchanges).fadeIn("slow");
+	}, {{ $refreshrate }});
 
-		$( document ).ready(function() {
-			var ajaxcasvddashboardincidents = '<?php echo URL::route('ajaxcasvddashboardincidents') ?>';
-			$('#ajaxcasvddashboardincidents').load(ajaxcasvddashboardincidents).fadeIn("slow");
-			
-			var ajaxcasvddashboardrequests = '<?php echo URL::route('ajaxcasvddashboardrequests') ?>';
-			$('#ajaxcasvddashboardrequests').load(ajaxcasvddashboardrequests).fadeIn("slow");
-			
-			var ajaxcasvddashboardchanges = '<?php echo URL::route('ajaxcasvddashboardchanges') ?>';
-			$('#ajaxcasvddashboardchanges').load(ajaxcasvddashboardchanges).fadeIn("slow");
+	$(document).ready(function () {
+		var ajaxcasvddashboardincidents = '<?php echo URL::route('ajaxcasvddashboardincidents') ?>';
+		$('#ajaxcasvddashboardincidents').load(ajaxcasvddashboardincidents).fadeIn("slow");
 
-			var timeframe="today";
+		var ajaxcasvddashboardrequests = '<?php echo URL::route('ajaxcasvddashboardrequests') ?>';
+		$('#ajaxcasvddashboardrequests').load(ajaxcasvddashboardrequests).fadeIn("slow");
 
-			var ajaxcasvddashboardtotalincidents = '<?php echo @Config::get('app.url') ?>';
-			ajaxcasvddashboardtotalincidents += ('/ajaxcasvddashboardtotalincidents/' + timeframe);
-			$('#incidentcount').load(ajaxcasvddashboardtotalincidents).fadeIn("slow");
-		});
+		var ajaxcasvddashboardchanges = '<?php echo URL::route('ajaxcasvddashboardchanges') ?>';
+		$('#ajaxcasvddashboardchanges').load(ajaxcasvddashboardchanges).fadeIn("slow");
 
-		function seltimeframe(timeframe) {
-			if (timeframe=="custom") {
-				document.getElementsByClassName("daterange")[0].setAttribute("style", "");
-				document.getElementsByClassName("daterange_btn")[0].setAttribute("style", "");
-			}
-		};
+	});
 
-		function settimeframe() {
-			var timeframe = document.getElementById("timeframe").value;
-			var itemtype = document.getElementById("itemtype").value;
-			var ajaxcasvddashboardtotalincidents = '<?php echo @Config::get('app.url') ?>';
-			ajaxcasvddashboardtotalincidents += ('/ajaxcasvddashboardtotalincidents/' + timeframe);
+	$(function() {
+		var start = moment().subtract(29, 'days');
+		var end = moment();
 
-			switch (itemtype) {
-				case "incident":
-					$('#incidentcount').load(ajaxcasvddashboardtotalincidents).fadeIn("slow");
-					break;
-				case "request":
-					$('#requestcount').load(ajaxcasvddashboardtotalrequests).fadeIn("slow");
-					break;
-				case "change":
-					$('#changecount').load(ajaxcasvddashboardtotalchanges).fadeIn("slow");
-					break;
-				case "all":
-					$('#incidentcount').load(ajaxcasvddashboardtotalincidents).fadeIn("slow");
-					$('#requestcount').load(ajaxcasvddashboardtotalrequests).fadeIn("slow");
-					$('#changecount').load(ajaxcasvddashboardtotalchanges).fadeIn("slow");
-					break;
-			}
+		function cb(start, end) {
+			$('#incidentrange span').html(start.format('MMM D, YYYY') + ' - ' + end.format('MMM D, YYYY'));
 		}
+
+		$('#incidentrange').daterangepicker({
+		startDate: start,
+		endDate: end,
+		ranges: {
+			'Today': [moment(), moment()],
+			'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+			'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+			'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+			'This Month': [moment().startOf('month'), moment().endOf('month')],
+			'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+		}
+		}, cb);
+
+		cb(start, end);
+	});
+
+	$(function() {
+		var start = moment().subtract(29, 'days');
+		var end = moment();
+
+		function cb(start, end) {
+		$('#requestrange span').html(start.format('MMM D, YYYY') + ' - ' + end.format('MMM D, YYYY'));
+		}
+
+		$('#requestrange').daterangepicker({
+		startDate: start,
+		endDate: end,
+		ranges: {
+			'Today': [moment(), moment()],
+			'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+			'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+			'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+			'This Month': [moment().startOf('month'), moment().endOf('month')],
+			'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+		}
+		}, cb);
+
+		cb(start, end);
+	});
+
+	$(function() {
+		var start = moment().subtract(29, 'days');
+		var end = moment();
+
+		function cb(start, end) {
+			$('#changerange input').html(start.format('MMM D, YYYY') + ' - ' + end.format('MMM D, YYYY'));
+			// var el = document.getElementById("changerangediv");
+			// el.value = (start.format('MMM D, YYYY') + ' - ' + end.format('MMM D, YYYY'));
+		}
+
+		$('#changerange').daterangepicker({
+		startDate: start,
+		endDate: end,
+		ranges: {
+			'Today': [moment(), moment()],
+			'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+			'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+			'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+			'This Month': [moment().startOf('month'), moment().endOf('month')],
+			'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+		}
+		}, cb);
+
+		cb(start, end);
+	});
+
+	function changerangeFunction() {
+		alert("test");
+	}
 
 </script>
 
